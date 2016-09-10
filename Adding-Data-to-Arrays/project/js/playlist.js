cb06baeeae8feea32e451a1d1e45bb5c19b63de8
Amanda Free <amandafree@Amandas-MacBook-Pro.local>
2016-09-10 18:38:28 -0400

    JavaScript: Loops, Arrays, and Objects

---
+++ Adding-Data-to-Arrays/project/js/playlist.js
@@ -0,0 +1,6 @@
+var playList = [];
+playList.push("I did it my way");
+playList.push("Respect", "Imagine");
+playList.unshift("Born to Run");
+playList.unshift("Louie, Louie", "Formation");
+printList(playList);
